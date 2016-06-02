b9236c8f3e3114d5b4b6eeb4af706ad242147b1a
russ1111 <rschneider77@gmail.com>
2016-06-01 23:17:42 -0700

    Update categories.php

--- categories.php
+++ categories.php
@@ -36,7 +36,17 @@ if(mysqli_num_rows($result) > 0)
 	while($row = mysqli_fetch_assoc($result))
 	{# process each row
         
-            echo '<div align="center"><a href="' . VIRTUAL_PATH . 'demo/science_news.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a></div>'; 
+	     	 if($row['CategoryID'] == 1){
+	+            echo '<div align="center"><a href="' . VIRTUAL_PATH . 'demo/science_news.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a></div>'; 
+	+        }
+	+        
+	+        elseif($row['CategoryID'] == 2){
+	+            echo '<div align="center"><a href="' . VIRTUAL_PATH . 'demo/world_news.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a></div>'; 
+	+        }
+	+        
+	+        else{
+	+            echo '<div align="center"><a href="' . VIRTUAL_PATH . 'demo/health_news.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Category']) . '</a></div>'; 
+	+        }
         
 	} 
     
